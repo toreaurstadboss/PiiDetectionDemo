47cb51dcf9a4c7de757f23606bc833f710f9fd7e
Tore Aurstad <tore.aurstad@gmail.com>
2024-04-21 22:14:14 +0200

    Init commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PiiDetectionDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
